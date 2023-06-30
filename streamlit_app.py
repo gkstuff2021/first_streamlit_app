91a6d3f0e156d7b5226429cd720e6227e84eebcb
gkstuff2021 <138247955+gkstuff2021@users.noreply.github.com>
2023-06-30 14:52:47 -0400

    Update streamlit_app.py
    more emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header("We will make Snowflake cool.")
 streamlit.text("Complete Badge 2 Asap.")
 streamlit.text("Aim for Certification.")
 streamlit.text("Your emojis for the day:🥣 🥗 🐔 🥑🍞")
+streamlit.header('🍌🥭 Build Your Own Training Plan 🥝🍇')
+
