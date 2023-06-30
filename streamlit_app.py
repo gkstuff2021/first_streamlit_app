6efde7d4060138e9aafb29fe2b0ee1400156a1a2
gkstuff2021 <138247955+gkstuff2021@users.noreply.github.com>
2023-06-30 14:58:16 -0400

    Update streamlit_app.py
    added pandas for fruitlist

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text("Aim for Certification.")
 streamlit.text("Your emojis for the day:🥣 🥗 🐔 🥑🍞")
 streamlit.header('🍌🥭 Build Your Own Training Plan 🥝🍇')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
