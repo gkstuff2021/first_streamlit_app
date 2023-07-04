a878aaad6cf591b1b623ddd318642a8deb2a947a
gkstuff2021 <138247955+gkstuff2021@users.noreply.github.com>
2023-07-04 18:18:52 -0400

    Update streamlit_app.py
    fixed syntax in insert row quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ streamlit.header("More Fruityvice Fruit Advice to add fruit of your choice...")
 
 def insert_row_snowflake(new_fruit):
     with  my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values (new_fruit)");
+        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit +"')");
         return "Thanks for adding:" + new_fruit
         
 
